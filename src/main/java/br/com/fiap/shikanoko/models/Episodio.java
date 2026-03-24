23a3d2bb535f00a1685f77aa08df8ee64986bbc9
LuzBGouveia <lu.barros.gouveia2@gmail.com>
2026-03-24 11:45:11 -0300

    feat: Adicionando atributos das classes Episodio.java e Personagem.java

--- src/main/java/br/com/fiap/shikanoko/models/Episodio.java
+++ src/main/java/br/com/fiap/shikanoko/models/Episodio.java
@@ -1,4 +1,12 @@
 package br.com.fiap.shikanoko.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Episodio {
+    private Long id;
+    private String title;
+    private String descricao;
 }
--- src/main/java/br/com/fiap/shikanoko/models/Personagem.java
+++ src/main/java/br/com/fiap/shikanoko/models/Personagem.java
@@ -1,4 +1,13 @@
 package br.com.fiap.shikanoko.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Personagem {
+    private Long id;
+    private String nome;
+    private String descricao;
+    private Boolean isCervo;
 }
