91330454a230f28ec3feedabae66202aa0caaaf7
EnzoOkuizumiFiap <enzookuizumifiap@gmail.com>
2026-03-25 09:47:09 -0300

    feat: Construindo todos os controllers

--- src/main/java/br/com/fiap/anime/controllers/AnimeController.java
+++ src/main/java/br/com/fiap/anime/controllers/AnimeController.java
@@ -1,7 +1,43 @@
 package br.com.fiap.anime.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import br.com.fiap.anime.models.Anime;
+import br.com.fiap.anime.services.AnimeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("animes")
 public class AnimeController {
+    @Autowired
+    private AnimeService service;
+
+    @GetMapping
+    public List<Anime> ListAll(){
+        return service.getAllAnimes();
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Anime> getAnimeById(@PathVariable Long id) {
+        return ResponseEntity.ok(service.getAnimeById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Anime> createAnime(@RequestBody Anime anime) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.addAnime(anime));
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Anime> updateAnime(@PathVariable Long id, @RequestBody Anime anime) {
+        return ResponseEntity.ok(service.updateAnime(id, Anime);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteAnime(@PathVariable Long id) {
+        service.deleteAnime(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/br/com/fiap/anime/controllers/PersonagemController.java
+++ src/main/java/br/com/fiap/anime/controllers/PersonagemController.java
@@ -1,4 +1,43 @@
 package br.com.fiap.anime.controllers;
 
+import br.com.fiap.anime.models.Personagem;
+import br.com.fiap.anime.services.PersonagemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("personagens")
 public class PersonagemController {
+    @Autowired
+    private PersonagemService service;
+
+    @GetMapping
+    public List<Personagem> ListAll(){
+        return service.getAllPersonagens();
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Personagem> getPersonagemById(@PathVariable Long id) {
+        return ResponseEntity.ok(service.getPersonagemById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Personagem> createPersonagem(@RequestBody Personagem Personagem) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.addPersonagem(Personagem));
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Personagem> updatePersonagem(@PathVariable Long id, @RequestBody Personagem Personagem) {
+        return ResponseEntity.ok(service.updatePersonagem(id, Personagem);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deletePersonagem(@PathVariable Long id) {
+        service.deletePersonagem(id);
+        return ResponseEntity.noContent().build();
+    }
 }
