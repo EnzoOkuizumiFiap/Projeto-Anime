cbcd9c2003c94edd0f932e7b7bca4051f9a0b794
EnzoOkuizumiFiap <enzookuizumifiap@gmail.com>
2026-04-23 09:14:02 -0300

    feat: DTO de anime, falta o resto, tipo mudar o Controller

---
+++ src/main/java/br/com/fiap/animes/Anime/DTO/AnimeRequest.java
@@ -0,0 +1,23 @@
+package br.com.fiap.animes.Anime.DTO;
+
+import br.com.fiap.animes.Anime.Anime;
+import br.com.fiap.animes.Anime.Categoria;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public record AnimeRequest(
+    String titulo,
+    String descricao,
+    LocalDate lancamento,
+    List<Categoria> categoria
+) {
+    public Anime toEntity() {
+        return Anime.builder()
+                .titulo(titulo)
+                .descricao(descricao)
+                .lancamento(lancamento)
+                .categoria(categoria)
+                .build();
+    }
+}
---
+++ src/main/java/br/com/fiap/animes/Anime/DTO/AnimeResponse.java
@@ -0,0 +1,23 @@
+package br.com.fiap.animes.Anime.DTO;
+
+import br.com.fiap.animes.Anime.Anime;
+import br.com.fiap.animes.Anime.Categoria;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public record AnimeResponse(
+    String titulo,
+    String descricao,
+    LocalDate lancamento,
+    List<Categoria> categoria
+) {
+    public static AnimeResponse fromEntity(Anime anime) {
+        return new AnimeResponse(
+                anime.getTitulo(),
+                anime.getDescricao(),
+                anime.getLancamento(),
+                anime.getCategoria()
+        );
+    }
+}
