2930a40d074b6d80ba7a8fd4f362052066d0be50
EnzoOkuizumiFiap <enzookuizumifiap@gmail.com>
2026-04-26 22:14:32 -0300

    feat: Atualizando o Validator

--- src/main/java/br/com/fiap/animes/validation/SemCaractereEspecialValidator.java
+++ src/main/java/br/com/fiap/animes/validation/SemCaractereEspecialValidator.java
@@ -2,12 +2,25 @@ package br.com.fiap.animes.validation;
 
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
+import org.springframework.stereotype.Component;
 
+@Component
 public class SemCaractereEspecialValidator implements ConstraintValidator<SemCaractereEspecial, String> {
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
         if (value == null || value.isBlank()) return true;
-        return value.matches("^[\\p{L}\\s]+$");
+
+        boolean ok = value.matches("^[a-zA-ZÀ-ÿ\\s]+$");
+
+        if (!ok) addViolation(context, "O campo não pode conter caracteres especiais");
+
+        return ok;
+    }
+
+    private void addViolation(ConstraintValidatorContext context, String message) {
+        context.disableDefaultConstraintViolation();
+        context.buildConstraintViolationWithTemplate(message)
+                .addConstraintViolation();
     }
 }
