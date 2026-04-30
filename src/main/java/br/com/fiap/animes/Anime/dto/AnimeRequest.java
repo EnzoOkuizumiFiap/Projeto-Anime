4077cba1f6d4606f7374b64be29fdaacb1441c82
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-30 09:33:15 -0300

    fix: Permitindo a criação de Anime com lista de personagens e temporadas vazia em AnimeRequest.

--- src/main/java/br/com/fiap/animes/Anime/dto/AnimeRequest.java
+++ src/main/java/br/com/fiap/animes/Anime/dto/AnimeRequest.java
@@ -2,13 +2,18 @@ package br.com.fiap.animes.Anime.dto;
 
 import br.com.fiap.animes.Anime.Anime;
 import br.com.fiap.animes.Anime.Categoria;
+import br.com.fiap.animes.Personagem.Personagem;
+import br.com.fiap.animes.Temporada.Temporada;
 import br.com.fiap.animes.validation.CategoriaValidation;
 import br.com.fiap.animes.validation.SemCaractereEspecial;
 import br.com.fiap.animes.validation.Titulo;
 import jakarta.validation.constraints.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+
+import static java.util.Objects.isNull;
 
 public record AnimeRequest(
         @NotBlank
@@ -26,7 +31,11 @@ public record AnimeRequest(
         @NotNull
         @Size(min = 1)
         @CategoriaValidation(enumClass = Categoria.class)
-        List<String> categoria
+        List<String> categoria,
+
+        List<Personagem> personagens,
+
+        List<Temporada> temporadas
 ) {
     public Anime toEntity() {
         return Anime.builder()
@@ -34,6 +43,8 @@ public record AnimeRequest(
                 .descricao(descricao)
                 .lancamento(lancamento)
                 .categoria(categoria.stream().map(c -> Categoria.valueOf(c.toUpperCase())).toList())
+                .personagens(isNull(personagens)? new ArrayList<>() : personagens)
+                .temporadas(isNull(temporadas)? new ArrayList<>() : temporadas)
                 .build();
     }
 }
